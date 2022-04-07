16b7a45eb14423d977e508f8add59114e904cfda
doreshin <d-190-61-95@yandex.ru>
2022-04-07 23:05:14 +0300

    crazy move =P

--- src/main/java/ru/rtech/controller/UserController.java
+++ src/main/java/ru/rtech/controller/UserController.java
@@ -1,7 +1,7 @@
 package ru.rtech.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.rtech.entity.User;
@@ -13,7 +13,7 @@ public class UserController {
     @Autowired
     private UserServiceImpl userServiceImpl;
 
-    @GetMapping("/user/{id}")
+    @PatchMapping("/user/{id}")
     public User getUser(
             @PathVariable("id") Integer id) {
         return userServiceImpl.getUser(id);
